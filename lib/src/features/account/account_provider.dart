7b32346a60779b22b26c626dd80183d951ee4775
Revin Sicat <SicatRevin@outlook.com>
2026-04-19 18:49:27 +0800

    commit: init account_provider

---
+++ lib/src/features/account/account_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'account.dart';
+import 'account_service.dart';
+
+final accountServiceProvider = Provider<AccountService>((ref) {
+    return AccountService();
+});
+
+final accountListProvider = FutureProvider<List<Account>>((ref) async {
+    final service = ref.read(accountServiceProvider);
+    return service.getAccountList();
+});
